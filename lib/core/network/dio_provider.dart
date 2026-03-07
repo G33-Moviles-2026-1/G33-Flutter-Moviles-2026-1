557fcafddf537d1dc1411d01cd8ce2b6b5161f71
Daniel <df.ortizv1@uniandes.edu.co>
2026-03-07 13:35:42 -0500

    Provider

---
+++ lib/core/network/dio_provider.dart
@@ -0,0 +1,29 @@
+import 'package:cookie_jar/cookie_jar.dart';
+import 'package:dio/dio.dart';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'network_constants.dart';
+
+final cookieJarProvider = Provider<CookieJar>((ref) {
+  return CookieJar();
+});
+
+final dioProvider = Provider<Dio>((ref) {
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: NetworkConstants.baseUrl,
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 10),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    ),
+  );
+
+  dio.interceptors.add(
+    CookieManager(ref.read(cookieJarProvider)),
+  );
+
+  return dio;
+});
