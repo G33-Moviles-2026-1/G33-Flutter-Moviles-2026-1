e2eba50a658f64534858fe22430169c7c0402180
Cesar-Avellaneda <105082120+Cesar-Avellaneda@users.noreply.github.com>
2026-05-22 22:04:02 -0500

    fix import

--- lib/features/auth/presentation/pages/profile_page.dart
+++ lib/features/auth/presentation/pages/profile_page.dart
@@ -3,6 +3,7 @@ import 'package:andespace/core/navigation/app_routes.dart';
 import 'package:andespace/core/navigation/app_tab.dart';
 import 'package:andespace/features/auth/domain/entities/user_status.dart';
 import 'package:andespace/features/auth/presentation/notifiers/auth_notifier.dart';
+import 'package:andespace/shared/theme/app_theme_extension.dart';
 import 'package:andespace/shared/widgets/app_scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
